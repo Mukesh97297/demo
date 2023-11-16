1207c15fb7ac8e50ad40ca68daed080ad10ea72a
Mukesh <mkverma97297@gmail.com>
2023-11-16 17:59:35 +0530

    comment

--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
-'use client'
-import Image from 'next/image'
-import{ Header} from './pages/navbar/page'
+// 
 
 
 export default function Home() {
   return (
     <div>
-      <Header/>
+     
     </div>
   )
 }
