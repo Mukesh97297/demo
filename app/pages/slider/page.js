dee9c041af87fe4b37bca0e80aff9ab7bec01a77
Mukesh <mkverma97297@gmail.com>
2023-11-16 17:49:50 +0530

    first

---
+++ app/pages/slider/page.js
@@ -0,0 +1,18 @@
+// 'use client'
+// import React from 'react'
+// import Carousel from 'react-multi-carousel'
+// import 'react-multi-carousel/lib/styles.css';
+// const SlickCarousel = () => {
+//     return (
+//         <div>
+//             <Carousel>
+//                 <div>Item 1</div>
+//                 <div>Item 2</div>
+//                 <div>Item 3</div>
+//                 <div>Item 4</div>
+//             </Carousel>
+//         </div>
+//     )
+// }
+
+// export default SlickCarousel
